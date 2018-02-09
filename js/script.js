47340aa0ff89853f1ea56870bc3edf8c2d8f05cb
Andreea Uta <dreea.9965@gmail.com>
2018-02-08 20:11:23 -0500

    added animations

--- js/script.js
+++ js/script.js
@@ -16,13 +16,39 @@ $(document).ready(function() {
 	$( ".small-line" ).hide();
 	$( ".medium-line" ).hide();
 
-
 	$(document).scroll(function() {
 	    if($(this).scrollTop() > categoriesTop ) {
 	      $(".small-line").show().addClass('animated');
 				$(".medium-line").show().addClass('animated');
 
 	    }
+	});
+
+	var featured1 = $(".featured1").offset().top;
+	var featured2 = $(".featured2").offset().top;
+	var featured3 = $(".featured3").offset().top;
+	var featured4 = $(".featured4").offset().top;
+
+
+	$( ".featured1" ).hide();
+	$( ".featured2" ).hide();
+	$( ".featured3" ).hide();
+	$( ".featured4" ).hide();
+
+	$(document).scroll(function() {
+			if($(this).scrollTop() > featured1 ) {
+				$(".featured1").show().addClass('animated');
+			}
+			if($(this).scrollTop() > featured2 ) {
+				$(".featured2").show().addClass('animated');
+			}
+			if($(this).scrollTop() > featured3 ) {
+				$(".featured3").show().addClass('animated');
+			}
+			if($(this).scrollTop() > featured4 ) {
+				$(".featured4").show().addClass('animated');
+			}
+
 	});
 
 });
